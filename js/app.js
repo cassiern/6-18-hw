da952f686caadf48dcdffe0963e4f8077426b8d5
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-18 16:59:43 -0600

    Completed homework assignment

---
+++ js/app.js
@@ -0,0 +1,54 @@
+/* 1. The difference between concatenation and interpolation 
+is that interpolation you use `${variable}` inside your
+console.log. In concatenation inside your
+console.log you use '' to write what you want
+to see, with + variable to render what you
+want. Example would be
+age = 21 
+console.log('I am ' + age) to render 
+'I am 21'. 
+
+2. DRY stands for DON'T REPEAT YOURSELF.
+We should pay attention to it becasue it helps
+eleminate bugs in your code. We have learned
+the for and while loop to help keep code DRY.
+
+3. When we declare a variable we are giving
+it a name that we can use to reference
+it at any point in our code. When we assign
+a value to a variable, we are assigning information
+to the name of that variable. A variable is 
+defined by using the = sign. 
+
+4. We should usually use const as with this
+variable we know it is set and protected from
+ever being changed. We should use let when 
+we know we want the variable to change, 
+like in a for loop.
+
+5. A parent directory is the top folder
+that hosts other folders and files inside 
+it, known as children.
+
+6. We can find hoow to use the command line
+by typing man [the command] and enter. 
+This will provide us with the manual on 
+that command. 
+
+7. Tab completion is used to type commands
+into terminal quicker by starting the 
+command, hitting tab, and having it complete
+the file/folder name you were wanting.
+
+
+
+
+
+
+
+
+
+
+
+
+
