dfc984028dd31e703a0c1e5d314dda0677e1e2f3
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-18 17:51:12 -0600

    Final completed Homework for 6-18

--- js/app.js
+++ js/app.js
@@ -39,6 +39,108 @@ that command.
 into terminal quicker by starting the 
 command, hitting tab, and having it complete
 the file/folder name you were wanting.
+*/
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = "Kevin";
+
+console.log(a < b);
+console.log(c > d);
+console.log(c === c);
+console.log(a < b && b < c);
+console.log(a === a && a < d);
+console.log(e === 'Kevin');
+console.log(48 == '48');
+
+
+/* Part 3 Question 1. 
+This is an infinite loop because we didn't
+declare when the loop should stop. 
+ 		  Question 2.
+This question isn't an infinte loop because we 
+declared that runProgram False which it only runs
+if it is true. 
+*/
+
+
+//We are assigning "A" to the variable 'letters'.
+/*let letters = "A";
+//We are saying the variable 'i' has a value of 0.
+*/
+
+let i = 0;
+//While i is less than 20, 
+
+while (i < 20) {
+	//add 1 "A" to "A".
+	letters += "A";
+	i++;
+}
+//show end result of the variable 'letters'
+console.log(letters);
+//It is showing that letters is not defined...
+
+/*
+Part 4 Question 1:
+While loops are boolean. Meaning, while they are true,
+they'll run. 
+For loops are ran with a specific end number in mind.
+*/
+
+
+for (i = 0; i <= 999; i++) {
+	console.log(i);
+}
+
+/*
+ Part 4 Question 3:
+The first part of the statement is declaring the
+variable 'i' is 0.
+The second part states that while 'i' is less than 100,
+console.log('phrase'). And until 'i' is 100, keep add 1
+to 'i'.
+*/
+
+
+for (let i = 999; i > 0; i--) {
+	console.log(i);
+}
+
+for (i = 1; i <= 10; i++) {
+	console.log(`The value of i is: ${i} of 10`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
